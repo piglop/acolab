66b8d80a41b0c0c2a838650c238619ca3f1093e6
Michael Witrant <mike@lepton.fr>
2013-02-17 10:08:12 +0100

    indented nav list

--- lib/toc_renderer.rb
+++ lib/toc_renderer.rb
@@ -3,15 +3,15 @@ class TocRenderer < Redcarpet::Render::HTML_TOC
   
   def doc_header
     @header_count = 0
-    %Q{<ul class="nav nav-list">}
+    %Q{<div class="well"><ul class="nav nav-list"><li class="nav-header">#{I18n.t "toc"}</li>}
   end
   
   def doc_footer
-    %Q{</ul>}
+    %Q{</ul></div>}
   end
   
   def header(text, level)
-    result = %Q{<li><a href="#toc_#@header_count"><i class="icon-chevron-right"></i> #{h text}</a></li>}
+    result = %Q{<li class="level-#{level}"><a href="#toc_#@header_count">#{h text}</a></li>}
     @header_count += 1
     result
   end
