e355e45122321577d4419f9a2a13beb417088e60
Michael Witrant <mike@lepton.fr>
2013-02-18 07:41:02 +0100

    increased image size

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,8 @@
 class Image < ActiveRecord::Base
   attr_accessible :file
+  
   has_attached_file :file,
-      :styles => { :thumbnail => "260x180>", :full => "700>" },
+      :styles => { :thumbnail => "260x180>", :full => "860>" },
       :convert_options => {:thumbnail => "-gravity center -extent 260x180"}
 
   belongs_to :owner, class_name: "User"
