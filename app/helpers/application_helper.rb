818d34b3bf75db5c3133ed848f4000f46395c711
Michael Witrant <mike@lepton.fr>
2013-03-02 11:44:38 +0100

    article form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,22 @@ module ApplicationHelper
       link_to(name, user)
     end
   end
+  
+  
+  # See http://blog.chrisblunt.com/rails-getting-the-id-of-form-fields-inside-a-fields_for-block/
+  def sanitized_object_name(object_name)
+    object_name.gsub(/]\[|[^-a-zA-Z0-9:.]/,"_").sub(/_$/,"")
+  end
+  
+  def sanitized_method_name(method_name)
+    method_name.sub(/\?$/, "")
+  end
+  
+  def form_tag_id(object_name, method_name)
+    "#{sanitized_object_name(object_name.to_s)}_#{sanitized_method_name(method_name.to_s)}"
+  end
+
+  def form_tag_name(object_name, method_name)
+    "#{sanitized_object_name(object_name.to_s)}[#{sanitized_method_name(method_name.to_s)}]"
+  end
 end
