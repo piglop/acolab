0f9779d89e21c178fab7175cfde613c5ac999f27
Michael Witrant <mike@lepton.fr>
2013-03-02 14:58:49 +0100

    display second headers in toc

--- lib/toc_renderer.rb
+++ lib/toc_renderer.rb
@@ -5,19 +5,39 @@ class TocRenderer < Redcarpet::Render::HTML_TOC
     text.parameterize
   end
   
+  def initialize(*args)
+    super
+    @headers = []
+  end
+  
   def doc_header
     ""
   end
   
   def doc_footer
-    ""
+    output = ""
+    @headers.each do |text, sub|
+      output << %Q{<li><a href="##{text.parameterize}">#{h text}</a>}
+      if sub.any?
+        output << "<ul>"
+        sub.each do |sub_text|
+          output << %Q{<li><a href="##{sub_text.parameterize}">#{h sub_text}</a></li>}
+        end
+        output << "</ul>"
+      end
+      output << %Q{</li>}
+    end
+    output
   end
   
   def header(text, level)
-    if level == 1
-      %Q{<li><a href="##{text.parameterize}">#{h text}</a></li>}
-    else
-      nil
+    case level
+    when 1
+      @headers << [text, []]
+    when 2
+      @headers << [text, []] if @headers.empty?
+      @headers.last.last << text
     end
+    ""
   end
 end
