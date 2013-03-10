b7408bc8c4b7f474db9121ec607fdd0f7dc1988e
Michael Witrant <mike@lepton.fr>
2013-03-10 18:07:04 +0100

    to_fund state

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -1,7 +1,7 @@
 class Workshop < ActiveRecord::Base
   attr_accessible :content, :description, :name, :state
   
-  STATES = %w( considered working abandoned )
+  STATES = %w( considered to_fund working abandoned )
 
   has_paper_trail
 end
